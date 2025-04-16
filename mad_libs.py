53c0e0c73c6f5172968da27dcddea8c56e865014
Nameera Siddiqui <nameerasiddiqui11@gmail.com>
2025-04-16 13:43:49 +0500

    madlib project 01

---
+++ mad_libs.py
@@ -0,0 +1,9 @@
+adj = input("Enter an adjective: ") #sbse pehle hamne koi bhi adjective yaha enter krna hai 
+verb1 = input("Enter a verb: ") #phr ek verb
+verb2 = input("Enter another verb: ") # phr ek or verb 
+famous_person = input("Enter a famous person: ") #kisi famous bnde ka naam 
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because "  \
+        f"I Love to {verb1}. and hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
